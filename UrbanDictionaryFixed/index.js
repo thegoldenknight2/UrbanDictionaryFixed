f6f7997510288454368648dd7f0803d4265829e1
thegoldenknight2 <garyspammer1@gmail.com>
2026-03-24 00:36:43 +0000

    Deploying to gh-pages from @ thegoldenknight2/UrbanDictionaryFixed@bf6cb1d8ee89daa982a64f698ae91fbc83888e69 🚀

--- UrbanDictionaryFixed/index.js
+++ UrbanDictionaryFixed/index.js
@@ -1,1 +1,7 @@
-(function(e,o,n){"use strict";const{FormText:t}=n.Forms;function l(){return React.createElement(t,null,"Hello, world!")}var r={onLoad:function(){o.logger.log("Hello world!")},onUnload:function(){o.logger.log("Goodbye, world.")},settings:l};return e.default=r,Object.defineProperty(e,"__esModule",{value:!0}),e})({},vendetta,vendetta.ui.components);
+(function(n,d,u){"use strict";const{sendBotMessage:o}=u.findByProps("sendBotMessage");let a;var c={onLoad(){a=d.registerCommand({name:"urban",displayName:"urban",description:"Look up a word on Urban Dictionary",displayDescription:"Look up a word on Urban Dictionary",type:1,inputType:1,options:[{name:"word",displayName:"word",description:"The word to look up",displayDescription:"The word to look up",type:3,required:!0}],execute:async function(s,t){const i=s.find(function(e){return e.name==="word"})?.value;if(i)try{const e=(await(await fetch(`https://api.urbandictionary.com/v0/define?term=${encodeURIComponent(i)}`)).json())?.list?.[0];if(!e){o(t.channel.id,`\u274C No definition found for **${i}**.`);return}const p=e.definition.replace(/\[|\]/g,""),r=e.example?.replace(/\[|\]/g,"").trim();o(t.channel.id,`\u{1F4D6} **${e.word}**
+
+${p}${r?`
+
+*${r}*`:""}
+
+\u{1F44D} ${e.thumbs_up}  \u{1F44E} ${e.thumbs_down}`)}catch{o(t.channel.id,"\u274C Failed to fetch definition. Try again!")}}})},onUnload(){a?.()}};return n.default=c,Object.defineProperty(n,"__esModule",{value:!0}),n})({},vendetta.commands,vendetta.metro);
